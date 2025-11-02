1861a14a002c7ba17572e8e0d2bd4f2b0c3c32ce
jkibura <167850144+jkibura@users.noreply.github.com>
2025-11-02 13:29:04 +0300

    Fix deployed app Notes fetch issue: Corrected a typo in my fetch url name. Yep! goodness me.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://mern-thinkboard-r38m.onrender.comg/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://mern-thinkboard-r38m.onrender.com/api"
 const api = axios.create({
     baseURL: BASE_URL,
 })
+
+console.log(BASE_URL)
 
 export default api
